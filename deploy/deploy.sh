fa1f49b18954826e5ece404bc45af3bdbbf2a61f
Jimmy Kuruvilla <jimmyjk@gmail.com>
2016-09-24 18:34:15 -0500

    Woot

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-aws s3 sync ../dist/ s3://webbcontracting.s3-website-us-east-1.amazonaws.com/  --delete --region us-east-1
+aws s3 sync ../app/ s3://webbcontracting  --delete --region us-east-1 --profile freelance
